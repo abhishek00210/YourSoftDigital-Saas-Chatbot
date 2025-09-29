5b3acc2202e7ee82812aee6b4fd77ecccf735319
Abhishek Kumar Rai <75179538+abhishek00210@users.noreply.github.com>
2025-09-29 17:56:16 +0530

    fixing cros origin

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,22 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        // This applies the headers to all routes in your /api/ folder
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Allows all domains for now
+          // For better security in production, you can restrict it to your domain:
+          // { key: "Access-Control-Allow-Origin", value: "https://preprod.yoursoftdigital.ca" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
